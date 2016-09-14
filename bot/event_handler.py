249d244f9de173f147aaa05a0d656ef7f825be47
codykeeling <codykeeling@carfax.com>
2016-09-14 08:49:20 -0500

    return imgur if things mess up

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -5,6 +5,9 @@ import re
 import urllib
 import pprint
 import random
+from imgurpython import ImgurClient
+import random
+
 
 logger = logging.getLogger(__name__)
 
@@ -112,10 +115,20 @@ class RtmEventHandler(object):
         pp.pprint(map)
 
         if len(map['data']) == 0:
-            return "http://i.imgur.com/epXFh5B.gifv"
+            client_id = "fccd85bd2df2d1d"
+            client_secret = "5d048f197e04703b1491995dcb8af50fd0ea34b5"
+
+            client = ImgurClient(client_id, client_secret)
+            items = client.get_album_images('NLqMb')
+
+            link = items[random.randint(0,len(items)-1)].link
+            if(link.endswith('gif')):
+                link = link.replace('.gif', '.gifv')
+                return link
+
+            return link
 
         url_string = map['data'][0]['images']['downsized']['url']
-
 
         return url_string
 
